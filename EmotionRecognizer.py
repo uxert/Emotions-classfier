149a18438a00ee00d61d40bada40b99ce76a96f6
uxert <uxert@wp.pl>
2025-01-28 17:59:34 +0100

    Enable the 3-rd convolutional layer, tune down the dropout a bit

--- EmotionRecognizer.py
+++ EmotionRecognizer.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class EmotionRecognizer(nn.Module):
-    def __init__(self, dropout_prob = 0.5, conv_dropout_prob = 0.3):
+    def __init__(self, dropout_prob = 0.4, conv_dropout_prob = 0.3):
         super(EmotionRecognizer, self).__init__()
         self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1)
         self.bn_conv1 = nn.LazyBatchNorm2d()
@@ -12,7 +12,7 @@ class EmotionRecognizer(nn.Module):
         self.bn_conv3 = nn.LazyBatchNorm2d()
 
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.LazyLinear(out_features=128)
+        self.fc1 = nn.LazyLinear(out_features=256)
         self.bn_fc_1 = nn.LazyBatchNorm1d()
         self.fc2 = nn.LazyLinear(out_features=8)  # 8 emotion classes
 
@@ -24,8 +24,8 @@ class EmotionRecognizer(nn.Module):
         x = self.pool(F.relu(self.bn_conv1(self.conv1(x))))
         x = self.conv_dropout(x)
         x = self.pool(F.relu(self.bn_conv2(self.conv2(x))))
-        # x = self.conv_dropout(x)
-        # x = self.pool(F.relu(self.bn_conv3(self.conv3(x))))
+        x = self.conv_dropout(x)
+        x = self.pool(F.relu(self.bn_conv3(self.conv3(x))))
         x = self.after_conv_dropout(x)
         x = x.view(x.size(0), -1)  # Flatten
 
