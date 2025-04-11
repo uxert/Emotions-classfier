8975520bcc5e3cdff6faea573127730d0deb9304
uxert <uxert@wp.pl>
2025-04-11 16:48:05 +0200

    Update function display_audio_and_predictions
    It now uses the make_one_prediction method from the model class, making it much shorter and more readable.
    
    I know it's not used anywhere in the project for now, but I don't have the heart to remove it completely.

--- emotions_classifier/utils/misc.py
+++ emotions_classifier/utils/misc.py
@@ -6,7 +6,7 @@ import gdown
 import os
 
 
-def display_audio_and_predictions(model, dataset: RAVDESSDataset, class_names, num_samples=5):
+def display_audio_and_predictions(model: EmotionRecognizer, dataset: RAVDESSDataset, class_names, num_samples=5):
     """
     Displays audio files and prints model predictions (console version with audio playback).
 
@@ -18,28 +18,17 @@ def display_audio_and_predictions(model, dataset: RAVDESSDataset, class_names, n
     """
     # Set the model to evaluation mode
     model.eval()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
 
     print("Displaying audio files and model predictions:")
     for i in range(num_samples):
         # Select a random sample from the dataset
-        idx = torch.randint(0, len(dataset), (1,)).item()
-        mel_spec, label = dataset[idx]
-
-        # Pass the sample through the model
-        mel_spec = mel_spec.unsqueeze(0).to(device)  # Add batch dimension
-        with torch.no_grad():
-            output = model(mel_spec)
-            predicted_class = output.argmax(dim=1).item()
-
+        true_emotion, predicted_emotion, audio_path = model.make_one_prediction(dataset, class_names)
         # Display the prediction
         print(f"\nSample {i + 1}/{num_samples}")
-        print(f"True Emotion: {class_names[label]}")
-        print(f"Predicted Emotion: {class_names[predicted_class]}")
+        print(f"True Emotion: {true_emotion}")
+        print(f"Predicted Emotion: {predicted_emotion}")
 
         # Play the audio file
-        audio_path = dataset.file_paths[idx]
         print(f"Playing audio file: {audio_path}")
         playsound(audio_path)
 
